ea4a6d8dd6b2f9a30c7c5c8acff191e504070a07
Sean Leffler <sean@errno.com>
2017-03-07 21:52:19 -0800

    Comments!

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,18 @@ use std::collections::HashMap;
 use bit_vec::BitVec;
 
 
+// A representation of a Smallfuck program. This isn't a very optimized
+// representation - since the runtime implementation is meant for checking the
+// type-level implementation, I decided it'd be best to just mirror the
+// type-level version as best as possible. So, we have a fairly conventional
+// AST here.
+//
+// - `Empty` - a program which does nothing. When the interpreter hits this, it
+//   halts if there's nothing left to run.
+// - `Left`/`Right` - pointer decrement/increment.
+// - `Flip` - flip the bit at the pointer.
+// - `Loop(P, Q)` - if the bit at the pointer is zero, run `P` then check again;
+//   else, continue with `Q`.
 #[derive(Debug)]
 pub enum Program {
     Empty,
@@ -19,6 +31,9 @@ pub enum Program {
 }
 
 
+// This is the `State` object output by `reify`ing the output of the type-level
+// implementation. We keep track of every bit in the type-level implementation,
+// so here we use a `BitVec` rather than a sparse representation.
 #[derive(Debug)]
 pub struct State {
     loc: usize,
@@ -26,6 +41,12 @@ pub struct State {
 }
 
 
+// The type-level implementation uses a zipper list, which means it doesn't have
+// to keep track of the pointer. However, in our runtime version, it'd be very
+// irritating if the pointer couldn't go negative. (I'm realizing now I could
+// have just let it wrap around, but screwit, I'm not gonna rewrite this.) So
+// we use a `HashMap` which allows us to map `isize` to `bool`
+// without much fanfare, efficiency, or effort.
 #[derive(Debug)]
 pub struct SparseState {
     loc: isize,
@@ -34,49 +55,56 @@ pub struct SparseState {
 
 
 impl Program {
+    // A "big step" here completely executes a Smallfuck program with respect to
+    // some given state.
     fn big_step(&self, state: &mut SparseState) {
         use self::Program::*;
 
         match *self {
-            Empty => {}
+            Empty => {
+                // No modifications to the program; stop recursing. We're done.
+            }
             Left(ref next) => {
+                // Pointer decrement.
                 state.loc -= 1;
                 println!("< | pointer from {} to {}. {:?}",
                          state.loc + 1,
                          state.loc,
                          state);
+                // We're done. Next instruction! Since we're representing our
+                // program with an AST-like format, we'll keep going using
+                // recursion.
                 next.big_step(state);
             }
             Right(ref next) => {
+                // Pointer increment.
                 state.loc += 1;
-                println!("> | pointer from {} to {}. {:?}",
-                         state.loc - 1,
-                         state.loc,
-                         state);
                 next.big_step(state);
             }
             Flip(ref next) => {
+                // Flip the bit at the pointer.
                 {
-                    let after_flip = {
-                        let cell = state.bits.entry(state.loc).or_insert(false);
-                        *cell = !*cell;
-                        *cell
-                    };
-                    println!("* | cell at pointer from {} to {}. {:?}",
-                             !after_flip,
-                             after_flip,
-                             state);
+                    // We're working with a `HashMap`; thus, an entry might not
+                    // exist. So, we insert `false` if it's not.
+                    let cell = state.bits.entry(state.loc).or_insert(false);
+                    *cell = !*cell;
                 }
                 next.big_step(state);
             }
             Loop(ref boxed) => {
+                // Loop - the most complex instruction. We check the bit at the
+                // pointer. If it's zero (`false`), we continue; if it's one
+                // (`true`), then we run the body of the loop again.
                 let &(ref block, ref next) = boxed.as_ref();
                 if *state.bits.get(&state.loc).unwrap_or(&false) {
-                    println!("[ | loop block. {:?}", state);
+                    // Case 1: zero - run the loop body.
                     block.big_step(state);
+                    // Looping means we re-check. Note that we're *recursing*
+                    // here, with the exact same program we started with, *but*
+                    // a different state.
                     self.big_step(state);
                 } else {
-                    println!("] | loop exit. {:?}", state);
+                    // Case 2: one - we continue onwards.
                     next.big_step(state);
                 }
             }
@@ -84,6 +112,8 @@ impl Program {
     }
 
 
+    // Convenience function to run a program without having to write out the
+    // state allocation boilerplate.
     fn run(&self) -> SparseState {
         let mut state: SparseState = SparseState {
             loc: 0,
@@ -97,9 +127,23 @@ impl Program {
 }
 
 
+// Here we get to the fun part! This is all done using the `type_operators!`
+// macro, which is defined in the [type-operators](https://crates.io/crates/type-operators)
+// crate. For more precise information, please read the documentation. I plan to
+// eventually make a blog post about this implementation to explain better in-depth.
 type_operators! {
+    // This weirdness is just a hack to make the macro work. Rust doesn't have
+    // the ability to generate arbitrary unique symbols in macros, so we provide
+    // a list of our own.
     [A, B, C, D, E]
     
+    // `ProgramTy` is the type-level representation of our Smallfuck program.
+    // This compiles to a trait definition - `ProgramTy`, with one function,
+    // `reify()`. `reify()` is defined inductively over these five types, as
+    // shown below - I think it's fairly readable and straightforward. `Empty`,
+    // `Left<P>`, `Right<P>`, `Flip<P>`, and `Loop<P, Q>` are all structs
+    // which are generated by the `type_operators!` macro along with the
+    // `ProgramTy` trait.
     concrete ProgramTy => Program {
         Empty => Program::Empty,
         Left(P: ProgramTy = Empty) => Program::Left(Box::new(P)),
@@ -108,23 +152,28 @@ type_operators! {
         Loop(P: ProgramTy = Empty, Q: ProgramTy = Empty) => Program::Loop(Box::new((P, Q))),
     }
 
+    // This compiles to a trait `Bit` and two unit structs - `F` and `T`.
+    // Once again, `reify` is added to the `Bit` trait and allows us to turn
+    // our `Bit`s into `bool`s. Useful for producing output to check against
+    // our runtime Smallfuck implementation!
     concrete Bit => bool {
         F => false,
         T => true,
     }
 
+    // We use a zipper list to represent the Smallfuck memory space. This is one
+    // side of that zipper list - it's a straightforward cons-list of bits. It
+    // can be `reify`'d into a `BitVec`.
     concrete List => BitVec {
         Nil => BitVec::new(),
         Cons(B: Bit, L: List = Nil) => { let mut tail = L; tail.push(B); tail },
     }
 
+    // `StateTy` is the type-level representation of the state of the Smallfuck
+    // interpreter. It's a zipper list, so we've got a left-list, `L` - our
+    // current bit, `C` - and our right-list, `R`.
     concrete StateTy => State {
         St(L: List, C: Bit, R: List) => {
-            // So, everything is actually backwards here. That's because we're
-            // looking at a zipper list here. So our `loc` 
-
-            println!("bits behind: {:?}, current bit: {}, bits ahead: {:?}", L, C, R);
-
             let mut bits = L;
             let loc = bits.len();
             bits.push(C);
@@ -137,6 +186,21 @@ type_operators! {
         },
     }
 
+    // This produces a trait `Running<StateTy>: ProgramTy` with an associated
+    // type `Output: StateTy`. `Run` is a type synonym defined as
+    // `type Run<P, S> = <P as Running<S>>::Output;`. 
+    //
+    // Now we get into a rather nasty bunch of case analysis. Heeere we go!
+    // - Pointer decrement, left cons-list is nil: create a new `F`
+    // - Pointer increment, right cons-list is nil: create a new `F`
+    // - Pointer decrement, left cons-list is cons: move the value out of the left cons-list
+    // - Pointer increment, right cons-list is cons: move the value out of the right cons-list
+    // - Flip current bit: current bit is `F` - change to `T`
+    // - Flip current bit: current bit is `T` - change to `F`
+    // - Loop, current bit is `F` - continue
+    // - Loop, current bit is `T` - run the body, then recursively run the same
+    //   instructions with the new state
+    // - Empty: return the state unmodified, and don't recurse
     (Run) Running(ProgramTy, StateTy): StateTy {
         forall (P: ProgramTy, C: Bit, R: List) {
             [(Left P), (St Nil C R)] => (# P (St Nil F (Cons C R)))
@@ -163,9 +227,11 @@ type_operators! {
 }
 
 
+// A Smallfuck state which is filled with `F` bits - a clean slate.
 pub type Blank = St<Nil, F, Nil>;
 
 
+// Convert nicely formatted Smallfuck into type-encoded Smallfuck.
 macro_rules! sf {
     (< $($prog:tt)*) => { Left<sf!($($prog)*)> };
     (> $($prog:tt)*) => { Right<sf!($($prog)*)> };
